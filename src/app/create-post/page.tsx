9b91332da0ce5df2a86d4e51ec13c509bd3b1f7c
Dimitar Mitev <d.mitev920123@gmail.com>
2024-05-23 11:44:30 +0300

    feat(): styling create post form

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -5,15 +5,25 @@ export default async function CreatePost() {
   return (
     <main className="text-center pt-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">Create post</h1>
-      <form action={createPost} className="h-10 space-x-2 mt-10">
+      <form
+        action={createPost}
+        className="flex flex-col max-w-[400px] mx-auto gap-2 my-10"
+      >
         <input
           type="text"
           name="title"
           placeholder="Title for new post"
           required
-          className="border rounded px-3 h-full"
+          className="border rounded px-3  h-10"
         />
-        <button className="h-full bg-blue-500 px-5 rounded text-white">
+        <textarea
+          name="body"
+          placeholder="Body content for new post"
+          className="border rounded px-3 py-2"
+          rows={6}
+          required
+        ></textarea>
+        <button className="h-10 bg-blue-500 px-5 rounded text-white">
           Create
         </button>
       </form>
